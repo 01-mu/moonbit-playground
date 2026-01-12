92ced438274b2fb9544c8235a369ea480c22b220
mu <01-mu@users.noreply.github.com>
2026-01-12 11:55:33 +0900

    feat: traffic logic on browser console

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 
 ```sh
-cp engine/target/wasm/release/build/src/main/main.wasm web/engine.was
+cd engine
+moon check
+moon build --target wasm
+
+cd ..
+find . -name "*.wasm"
+cp engine/target/wasm/release/build/src/main/main.wasm web/engine.wasm
+
+cd web
+python -m http.server 8000
 ```
--- web/main.js
+++ web/main.js
@@ -1,18 +1,12 @@
-const out = document.getElementById("out");
-const btn = document.getElementById("run");
+const { instance } = await WebAssembly.instantiateStreaming(
+  fetch("engine.wasm"),
+  {}
+)
 
-btn.addEventListener("click", async () => {
-  const res = await fetch("./engine.wasm");
-  const bytes = await res.arrayBuffer();
-  const { instance } = await WebAssembly.instantiate(bytes);
+let state = instance.exports.tl_init()
 
-  // MoonBit 側で link.wasm.exports に ["add"] を指定している前提
-  const add = instance.exports.add;
-  if (typeof add !== "function") {
-    out.textContent = `Export "add" not found. Exports: ${Object.keys(instance.exports).join(", ")}`;
-    return;
-  }
-
-  out.textContent = `add(2, 3) => ${add(2, 3)}`;
-});
-
+setInterval(() => {
+  state = instance.exports.tl_step(state, 100) // dt = 100ms
+  const light = instance.exports.tl_light(state)
+  console.log("light =", light) // 0/1/2
+}, 100)
